c55bd2e0acadf248b1c1202661ce52adcd7d76d6
Anish Thite <anishthite@live.com>
2017-06-13 00:23:55 -0400

    update names

--- playGUI.py
+++ playGUI.py
@@ -1,22 +1,25 @@
 #=====================footballGUI=========================
 #           Author: Anish Thite
 import tkinter
-
+from playlist import addplay
 #init playbook
-main = tkinter.Tk()
-main.geometry("800x400")
-main.title("Notre Dame Play Manager- Choose Play")
+playgui = tkinter.Tk()
+playgui.geometry("800x400")
+playgui.title("Notre Dame Play Manager- Choose Play")
 
 #define playbutton class
-class playbutton:
-    def __init__(self,master,message, rowint,col):
-        button = tkinter.Button(master, text = message, bg = "light green", padx = 50, pady = 50 )
+class playtype:
+    def __init__(self,master,message, odk, rowint,col):
+        button = tkinter.Button(master, text = message, bg = "light green", padx = 50, pady = 50, command = lambda: addplay(message) )
         button.grid(row = rowint, column = col, padx = 10, pady = 10)
 
-play1 = playbutton(main,"Defense",0,0)
-play2 = playbutton(main,"Kick Return",1,0)
-play3 = playbutton(main,"Kick Off",0,1)
-play4 = playbutton(main,"Early",1,1)
-play5 = playbutton(main,"Late",0,2)
-play6 = playbutton(main,"Doubles",1,2)
-main.mainloop()
+#Defensive plays
+play1 = playtype(playgui,"Defense", "D", 0,0)
+#Kicks
+play2 = playtype(playgui,"Kick Return", "K",  1,0)
+play3 = playtype(playgui,"Kick Off", "K", 0,1)
+#Offensive Formations
+play4 = playtype(playgui,"Early", "O", 1,1)
+play5 = playtype(playgui,"Late", "O", 0,2)
+play6 = playtype(playgui,"Doubles", "O", 1,2)
+playgui.mainloop()
