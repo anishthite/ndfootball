475f60bb32f8b05fba283e9f34532a64641ef7aa
Anish Thite <anishthite@live.com>
2017-06-17 23:46:12 -0400

    finished full prototype

--- playlist.py
+++ playlist.py
@@ -1,7 +1,7 @@
 #csv file management python
+from preplayGUI import preplay
 from playGUI import currentplay
 from resultGUI import playresult
-
 import csv
 
 currentgame = 'playlist.csv'
@@ -9,7 +9,14 @@ currentgame = 'playlist.csv'
 def addplay():
     outputFile = open(currentgame, 'a', newline = '')
     writer = csv.writer(outputFile)
-    writer.writerow([currentplay.odk,currentplay.formation,playresult.playtype, playresult.playend, playresult.gainloss])
+    writer.writerow([preplay.down,
+                    preplay.distance,
+                    preplay.hash,
+                    currentplay.odk,
+                    currentplay.formation,
+                    playresult.playtype, 
+                    playresult.playend, 
+                    playresult.gainloss])
     outputFile.close()
 
 addplay()
--- preplayGUI.py
+++ preplayGUI.py
@@ -5,23 +5,97 @@ import tkinter
 #init playbook
 preplaygui = tkinter.Tk()
 preplaygui.geometry()
-preplaygui.title("Notre Dame Play Manager- Choose Play")
+preplaygui.title("Notre Dame Play Manager- Choose PrePlay")
 
 #DEFINE PREPLAY CLASS USED TO INOUT DATA INTO PLAYLIST.PY
 class preplay:
-    
-class preplaytype:
-    def __init__(self,master,message, odk, rowint,col):
-        button = tkinter.Button(master, text = message, bg = "light green", padx = 50, pady = 50, command = lambda: addplay(message) )
-        button.grid(row = rowint, column = col, padx = 10, pady = 10)
+    def  __init__(self):
+        self.down = 0
+        self.distance = 0
+        self.hash = ""
+        self.ydline = 0
+    def addDown(self,number):
+        self.down = number
+    def addDistance(self,number):
+        self.distance += number
+    def addhash(self,message):
+        self.hash = message
+    def addydline(self,number):
+        self.ydline += number
+    def multiplyydline(self,number):
+        self.ydline *= number
 
-#Defensive plays
-play1 = preplaytype(preplaygui,"Defense", "D", 0,0)
-#Kicks
-play2 = preplaytype(preplaygui,"Kick Return", "K",  1,0)
-play3 = preplaytype(preplaygui,"Kick Off", "K", 0,1)
-#Offensive Formations
-play4 = preplaytype(preplaygui,"Early", "O", 1,1)
-play5 = preplaytype(preplaygui,"Late", "O", 0,2)
-play6 = preplaytype(preplaygui,"Doubles", "O", 1,2)
+#INITILIAZE VARIABLE TO STORE DATA
+preplay = preplay()
+class downbutton:
+    def __init__(self, master, message, rowint, col):
+        button = tkinter.Button(master, text = message, bg = "light green", padx = 20, pady = 20, command = lambda: preplay.addDown(message))
+        button.grid(row = rowint, column = col, padx = 5, pady = 5)
+        button.config(width = 5)
+class distancebutton:
+    def __init__(self, master, message, rowint,col, multiplyer):
+        button = tkinter.Button(master, text = message, bg = "light blue", padx = 20, pady = 20, command = lambda: preplay.addDistance((multiplyer*int(message))))
+        button.grid(row = rowint, column = col, padx = 5, pady = 5)
+        button.config(width = 5)
+class hashbutton:
+    def __init__(self, master, message, rowint,col):
+        button = tkinter.Button(master, text = message, bg = "yellow", padx = 20, pady = 20, command = lambda: preplay.addhash(message))
+        button.grid(row = rowint, column = col, padx = 5, pady = 5)
+        button.config(width = 5)
+class ydline:
+    def __init__(self, master, message, rowint, col, multiplyer):
+        button = tkinter.Button(master, text = message, bg = "light blue", padx = 20, pady = 20, command = lambda: preplay.addydline((multiplyer*int(message))))
+        button.grid(row = rowint, column = col, padx = 5, pady = 5)
+        button.config(width = 5)
+#Down buttons
+label1 = tkinter.Label(preplaygui, text = "Down", padx = 20, pady = 20 )
+label1.grid(row = 0, column = 0, padx = 5, pady = 5)
+down = ["0","1","2","3","4"]
+r = 1
+c = 0
+for d in down:
+    x = downbutton(preplaygui, d, r,c )
+    r +=1
+#distance buttons
+label2 = tkinter.Label(preplaygui, text = "Distance", padx = 20, pady = 20 )
+label2.grid(row = 0, column = c+1, padx = 5, pady = 5)
+numbers = ["0","1","2","3","4","5","6","7","8","9"]
+r = 1
+c +=1
+for n in numbers:
+    y = distancebutton(preplaygui, n, r,c,10)
+    r+=1
+r = 1
+c += 1
+for a in numbers:
+    b = distancebutton(preplaygui, a,r,c,1)
+    r+=1
+#hash buttons
+label3 = tkinter.Label(preplaygui, text = "hash", padx = 20, pady = 20 )
+label3.grid(row = 0, column = c+1, padx = 5, pady = 5)
+hashlist = ["call","far"]
+r = 1
+c +=1
+for h in hashlist:
+    v = hashbutton(preplaygui, h, r, c)
+    r+=1
+#ydline buttons
+label4 = tkinter.Label(preplaygui, text = "hash", padx = 20, pady = 20 )
+label4.grid(row = 0, column = c+1, padx = 5, pady = 5)
+r = 1
+c+=1
+#10s numbers
+for i in numbers:
+    u = distancebutton(preplaygui, i, r,c,10)
+    r+=1
+r = 1
+c += 1
+#1s numbers
+for q in numbers:
+    w = distancebutton(preplaygui, q,r,c,1)
+    r+=1
+#neg button initilization
+negbutton = tkinter.Button(preplaygui, text = "other's", bg = "red", padx = 20, pady = 20, command = lambda: preplay.multiplyydline(-1))
+negbutton.grid(row = 1, column = c + 1, padx = 5, pady = 5)
+negbutton.config(width = 5)
 preplaygui.mainloop()
