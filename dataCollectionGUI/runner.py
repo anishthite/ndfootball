0ed46120dfd997809c62c842f364c5171058b99e
Anish Thite <anishthite@live.com>
2017-06-22 00:39:01 -0400

    reformat

--- dataCollectionGUI/runner.py
+++ dataCollectionGUI/runner.py
@@ -3,7 +3,7 @@ import playlist
 breakFlag = False
 while breakFlag == False:
     newplay = playlist.playlist()
-    newplay.addplay('playlist.csv')
+    newplay.addplay('../playlist.csv')
     if (newplay.playresult.lastplay == True) :
         breakFlag = True
     
